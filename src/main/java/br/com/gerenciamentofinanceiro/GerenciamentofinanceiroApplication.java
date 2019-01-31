0562581809db388a762c8b0b320b119818bbdb47
Gustavo H. L. S. Zaffani <gustavo.zaffani@vms.com.br>
2019-01-31 16:16:15 -0200

    Initial commit

---
+++ src/main/java/br/com/gerenciamentofinanceiro/GerenciamentofinanceiroApplication.java
@@ -0,0 +1,14 @@
+package br.com.gerenciamentofinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentofinanceiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentofinanceiroApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/br/com/gerenciamentofinanceiro/GerenciamentofinanceiroApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.gerenciamentofinanceiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GerenciamentofinanceiroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
